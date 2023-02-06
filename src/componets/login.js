e991ac5aa8c2cf770a867c33c71ccdf96d223de5
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-06 12:15:05 +0100

    feat: update Login component

--- src/componets/login.js
+++ src/componets/login.js
@@ -3,9 +3,7 @@ import { signInWithGoogle } from "../services/google";
 export const Login = () => {
   return (
     <div>
-      <button className="button" onClick={signInWithGoogle}>
-        <i className="fab fa-google"></i>Sign in with google
-      </button>
+      <button onClick={signInWithGoogle}>Sign in with google</button>
     </div>
   );
 };
