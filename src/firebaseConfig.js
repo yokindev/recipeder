22b0da383836a447f7a3d3405907c40fdcaa975d
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-06 09:37:57 +0100

    feat: add firebase configuration

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBjDo_tUWT7yivP33rFLT8kQWaMZ0yjQWc",
+    authDomain: "recipeder.firebaseapp.com",
+    projectId: "recipeder",
+    storageBucket: "recipeder.appspot.com",
+    messagingSenderId: "978399328102",
+    appId: "1:978399328102:web:20de870a9254488994faa7"
+  };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { FirebaseAppProvider } from "reactfire";
+import { firebaseConfig } from "./firebaseConfig";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Suspense fallback={<p>Loading ...</p>}>
+      <App />
+    </Suspense>
+  </FirebaseAppProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
