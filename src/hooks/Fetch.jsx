b1d473c0a94f21a6b4d787313c5c808dbaf5d909
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-07 19:28:21 +0100

    feat: add Fetch hook

---
+++ src/hooks/Fetch.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export default function Fetch(url, options) {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(url, options);
+        const data = await res.json();
+        setResult(data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.log(error);
+      }
+    })();
+  }, [url, options]);
+
+  return {result, loading}
+}
