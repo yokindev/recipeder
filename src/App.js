c5ede12291760249383319d87360ef2c98d52bd0
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-09 13:46:30 +0100

    feat: update logic of the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
-import useFetch from "./hooks/useFetch";
 import { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
 
 function App() {
-  const [user, setUser] = useState(null);
   const auth = getAuth();
+
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -14,11 +14,7 @@ function App() {
     });
   }, [auth]);
 
-  const recipes = useFetch(
-    `${process.env.REACT_APP_API_URL}?type=public&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&diet=balanced&random=true`
-  );
-
-  return <>{user ? <Home recipes={recipes} /> : <Login user={user}/>}</>;
+  return <>{user ? <Home /> : <Login />}</>;
 }
 
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
+import useFetch from "../../hooks/useFetch";
 import { NavBar } from "./NavBar/NavBar";
 import { SearchBar } from "./SearchBar/SearchBar";
 
 export const Home = ({ recipes }) => {
-  // const { result } = recipes;
 
-  // if (result) {
-  //   const { hits } = result;
-  //   console.log(hits);
+  const defaultRecipes = useFetch(
+    `${process.env.REACT_APP_API_URL}?type=public&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&diet=balanced&random=true`
+  );
+  const { result } = defaultRecipes;
+
+  if (result) {
+    const { hits } = result;
+    console.log(hits);
 
   return (
     <>
@@ -15,3 +20,4 @@ export const Home = ({ recipes }) => {
     </>
   );
 };
+}
