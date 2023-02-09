14a3a9117a878fb110e6997f93d2546983c017f5
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-09 20:41:43 +0100

    feat: add functions to SearchBar

--- src/pages/Home/SearchBar/SearchBar.jsx
+++ src/pages/Home/SearchBar/SearchBar.jsx
@@ -6,13 +6,37 @@ import {
   SearchBarWrapper,
 } from "./SearchBar.styles";
 import IconSearch from "../../../assets/icons/search.svg";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const SearchBar = () => {
+  const navigate = useNavigate();
+
+  const [target, setTarget] = useState("");
+  localStorage.setItem("Target", target);
+
+  const handleKey = (e) => {
+    if (e.key === "Enter" && target) {
+      navigate("/reciperesults");
+    }
+  };
+
+  const search = () => {
+    if (target) {
+      navigate("/reciperesults");
+    }
+  };
+
   return (
     <SearchBarContainer>
       <SearchBarWrapper>
-        <SearchBarInput type="text" placeholder="Let's find out" />
-        <SearchBarButton>
+        <SearchBarInput
+          type="text"
+          placeholder="Let's find out"
+          onKeyDown={handleKey}
+          onChange={(e) => setTarget(e.target.value)}
+        />
+        <SearchBarButton onClick={search}>
           <SearchBarIcon src={IconSearch} />
         </SearchBarButton>
       </SearchBarWrapper>
