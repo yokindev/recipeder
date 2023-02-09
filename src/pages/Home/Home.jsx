52633a53a0b11e0ff9f509701298690e700b862d
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-09 20:44:43 +0100

    feat: add RecipeResults component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { NavBar } from "./NavBar/NavBar";
 import { SearchBar } from "./SearchBar/SearchBar";
 import { Default } from "./Default/Default";
 import { TopBar } from "./Home.styles";
+import { RecipeResults } from "./RecipeResults/RecipeResults";
 
 export const Home = () => {
   return (
@@ -16,6 +17,7 @@ export const Home = () => {
         <Routes>
           <Route path="/" element={<Default />} />
           <Route path="/home" element={<Default />} />
+          <Route path="/results" element={<RecipeResults />} />
         </Routes>
       </BrowserRouter>
     </>
