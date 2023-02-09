10639bff85b8bf6c6dd302e64d1abfc98e36b5d9
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-09 17:34:32 +0100

    feat: create SearchBar

--- src/pages/Home/SearchBar/SearchBar.jsx
+++ src/pages/Home/SearchBar/SearchBar.jsx
@@ -1,3 +1,21 @@
+import {
+  SearchBarButton,
+  SearchBarContainer,
+  SearchBarIcon,
+  SearchBarInput,
+  SearchBarWrapper,
+} from "./SearchBar.styles";
+import IconSearch from "../../../assets/icons/search.svg";
+
 export const SearchBar = () => {
-    
-}
+  return (
+    <SearchBarContainer>
+      <SearchBarWrapper>
+        <SearchBarInput type="text" placeholder="Let's find out" />
+        <SearchBarButton>
+          <SearchBarIcon src={IconSearch} />
+        </SearchBarButton>
+      </SearchBarWrapper>
+    </SearchBarContainer>
+  );
+};
---
+++ src/pages/Home/SearchBar/SearchBar.styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100px;
+  padding: 0px 20px 0px 20px;
+`;
+
+export const SearchBarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  border: 3px solid #34a0a4;
+  border-radius: 50px;
+  padding: 15px;
+`;
+
+export const SearchBarInput = styled.input`
+  width: 100%;
+  border: none;
+  font-weight: 900;
+  font-size: 20px;
+  outline: none;
+`;
+
+export const SearchBarButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
+
+export const SearchBarIcon = styled.img`
+  width: 25px;
+`;
