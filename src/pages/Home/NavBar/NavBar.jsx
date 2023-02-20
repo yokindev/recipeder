2482c28d977d0c7588073fbc209cbe7b752a39cc
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-20 12:31:51 +0100

    feat: update NavBar fetch

--- src/pages/Home/NavBar/NavBar.jsx
+++ src/pages/Home/NavBar/NavBar.jsx
@@ -11,27 +11,24 @@ import ImageLogo from "../../../assets/images/chef-hat.png";
 import IconLogout from "../../../assets/svg/power.svg";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 export const NavBar = ({ setData }) => {
-  const [url, setUrl] = useState(null);
   const navigate = useNavigate();
   const auth = getAuth();
 
   const links = ["American", "Asian", "Italian", "Mediterranean", "Mexican"];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(url);
-        const json = await res.json();
-        setData(json.hits);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, [url]);
+  const searchType = async (link) => {
+    try {
+      const res = await fetch(
+        `${process.env.REACT_APP_API_URL}?type=public&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&cuisineType=${link}`
+      );
+      const json = await res.json();
+      setData(json.hits);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <NavBarContainer>
@@ -40,14 +37,7 @@ export const NavBar = ({ setData }) => {
       </NavBarLogo>
       <NavBarLinks>
         {links.map((link, index) => (
-          <NavBarLink
-            key={index}
-            onClick={() =>
-              setUrl(
-                `${process.env.REACT_APP_API_URL}?type=public&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&cuisineType=${link}`
-              )
-            }
-          >
+          <NavBarLink key={index} onClick={() => searchType(link)}>
             {link}
           </NavBarLink>
         ))}
